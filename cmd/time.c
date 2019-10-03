f5884e375383e0dfd79a96a0d82f6faa88c3acde
Michael Scott <mike@foundries.io>
2019-10-03 16:57:17 -0700

    avnet: cmd: time changes
    Signed-off-by: Michael Scott <mike@foundries.io>

--- cmd/time.c
+++ cmd/time.c
@@ -5,6 +5,11 @@
 
 #include <common.h>
 #include <command.h>
+#include <uuid.h>
+#include <tpm-v2.h>
+#include <uboot_aes.h>
+#include <stdio.h>
+#include <fs.h>
 
 static void report_time(ulong cycles)
 {
@@ -29,14 +34,52 @@ static int do_time(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	ulong cycles = 0;
 	int retval = 0;
 	int repeatable = 0;
+	unsigned int random = 0;
+	// secret key part
+	uint8_t ScratchArea[] =
+	  { 0x72, 0x55, 0x49, 0x59, 0x14, 0x50, 0xDC, 0x2C, 0x9B, 0x59, 0xB7,
+	    0xA2, 0x75, 0x0D, 0x73, 0x87};
+	uint8_t RandomStr[AES_EXPAND_KEY_LENGTH];
+	uint8_t NewRandomStr[AES_EXPAND_KEY_LENGTH];
+	uint8_t OldRandomStr[AES_EXPAND_KEY_LENGTH];
+	uint8_t OldRandomAES[AES_EXPAND_KEY_LENGTH];
+	uint8_t key_exp[AES_EXPAND_KEY_LENGTH];
+	uint8_t ArgV3[16];
+	unsigned char GuidBin[16];
+	int OldCnt = 0;
+	int FileSize = 0;
+	char FileSizeStr[16];
+	memset(RandomStr, 0, sizeof(RandomStr));
+	memset(OldRandomStr, 0, sizeof(RandomStr));
+	memset(key_exp, 0, sizeof(key_exp));
 
-	if (argc == 1)
-		return CMD_RET_USAGE;
+	// Prep key
+	aes_expand_key(ScratchArea, key_exp);
 
-	retval = cmd_process(0, argc - 1, argv + 1, &repeatable, &cycles);
+	// Get new random value.
+	cycles = get_timer(0);
 	report_time(cycles);
+	srand((int)cycles);
+	random = rand();
+	gen_rand_uuid(&GuidBin);
+	random = random + GuidBin[16] + (GuidBin[15] << 8) + (GuidBin[14] << 16) + (GuidBin[13] << 24);
 
-	return retval;
+	memset(RandomStr, 0 , sizeof(RandomStr));
+	sprintf(RandomStr, "%X", random);
+
+	aes_encrypt(RandomStr, key_exp, NewRandomStr);
+
+	// change TPM2_RH_PLATFORM authorizations
+	tpm2_change_auth(TPM2_RH_PLATFORM, NewRandomStr, strlen(NewRandomStr), NULL, 0);
+	// For security clear out all working values
+	memset(NewRandomStr, 0, sizeof(NewRandomStr));
+	random = 0;
+	cycles = 0;
+	memset(RandomStr, 0, sizeof(RandomStr));
+	memset(OldRandomStr, 0, sizeof(RandomStr));
+	memset(key_exp, 0, sizeof(key_exp));
+	memset(GuidBin, 0 , sizeof(GuidBin));
+      return retval;
 }
 
 U_BOOT_CMD(time, CONFIG_SYS_MAXARGS, 0, do_time,
