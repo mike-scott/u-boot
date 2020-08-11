7e9d131c21b69adde4d59cec2e06ccce62b3bace
Padmarao Begari <padmarao.begari@microchip.com>
2020-08-11 13:45:30 +0530

    [FIO extras] include configs for the Microchip icicle-kit-es
    Signed-off-by: Padmarao Begari <padmarao.begari@microchip.com>
    [mike@foundries.io: rebased on 2020.10]
    Signed-off-by: Michael Scott <mike@foundries.io>

--- include/configs/microchip_mpfs_icicle.h
+++ include/configs/microchip_mpfs_icicle.h
@@ -34,9 +34,9 @@
 
 /*
  * Size of malloc() pool
- * 512kB is suggested, (CONFIG_ENV_SIZE + 128 * 1024) was not enough
+ * 8M is suggested, (CONFIG_ENV_SIZE + 128 * 1024) was not enough
  */
-#define CONFIG_SYS_MALLOC_LEN	(512 << 10)
+#define CONFIG_SYS_MALLOC_LEN	0x800000 /* 8 MB */
 
 /*
  * Physical Memory Map
@@ -48,12 +48,26 @@
 /* Init Stack Pointer */
 #define CONFIG_SYS_INIT_SP_ADDR	(CONFIG_SYS_SDRAM_BASE + 0x200000)
 
-#define CONFIG_SYS_LOAD_ADDR	0x80000000 /* SDRAM */
+#define CONFIG_SYS_LOAD_ADDR	(CONFIG_SYS_SDRAM_BASE + 0x200000) /* SDRAM */
 
 /*
  * memtest works on DRAM
  */
 
 /* When we use RAM as ENV */
+
+#define CONFIG_SYS_BOOTM_LEN        0x4000000 /* 64 MB */
+
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0)
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"fdt_high=0xffffffffffffffff\0" \
+	"initrd_high=0xffffffffffffffff\0" \
+	"kernel_addr_r=0x84000000\0" \
+	"fdt_addr_r=0x88000000\0" \
+	"scriptaddr=0x88100000\0" \
+	"pxefile_addr_r=0x88200000\0" \
+	"ramdisk_addr_r=0x88300000\0"
 
 #endif /* __CONFIG_H */
